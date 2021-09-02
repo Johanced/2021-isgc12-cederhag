5c5a4d99be914f47a65712492ed71e12233f5d24
Johanced <johan.cederhag@hotmail.se>
2021-09-02 16:35:35 +0200

    Lade till en Toast

--- Labb1/app/src/main/java/com/example/labb1/MainActivity.java
+++ Labb1/app/src/main/java/com/example/labb1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.labb1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast();
+    }
+
+    public void Toast(){
+        Toast.makeText(getApplicationContext(), "Hej där", Toast.LENGTH_SHORT).show();
     }
 }
