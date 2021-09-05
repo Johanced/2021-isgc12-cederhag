1725b45857c2a2ea6decb263ab2744399307eddc
Johanced <johan.cederhag@hotmail.se>
2021-09-05 13:34:03 +0200

    Labb 1 done v.1

--- Labb1/app/src/main/java/com/example/labb1/MainActivity.java
+++ Labb1/app/src/main/java/com/example/labb1/MainActivity.java
@@ -3,19 +3,38 @@ package com.example.labb1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ToastBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast();
+        addBtnListener();
+
+    }
+
+    public void addBtnListener(){
+
+        ToastBtn = (Button) findViewById(R.id.ToastBtn);
+
+        ToastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast();
+            }
+        });
     }
 
     public void Toast(){
-        Toast.makeText(getApplicationContext(), "Hej där", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_SHORT).show();
     }
+
+
 }
