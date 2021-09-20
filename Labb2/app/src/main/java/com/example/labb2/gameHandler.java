fbb931908308290d7b46029e0e36a1ff92a16598
Johanced <johan.cederhag@hotmail.se>
2021-09-21 01:19:34 +0200

    Labb2 3.0 90% done

--- Labb2/app/src/main/java/com/example/labb2/gameHandler.java
+++ Labb2/app/src/main/java/com/example/labb2/gameHandler.java
@@ -1,44 +1,68 @@
 package com.example.labb2;
 
+import android.content.Intent;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Vector;
 
-public class gameHandler {
+public class gameHandler extends AppCompatActivity {
     private final String TAG = "Main";
     private int failNmr = 0;
-    private final int maxRounds = 7;
     private String theWord;
-    private Vector<String> words;
+    private Vector<String> wordsLibrary;
+    public char[] unknownWord;
 
-    public gameHandler(){
-        words= new Vector<>();
+    public gameHandler(String difficulty){
+        wordsLibrary = new Vector<>();
         initWords();
+
 
     }
     public void initWords(){
-        words.add("dog");
-        words.add("cat");
-        words.add("table");
-        words.add("car");
-        words.add("baseball");
-        words.add("toyota");
-        words.add("pen");
-        words.add("window");
-        words.add("lasagne");
+        wordsLibrary.add("dog");
+        wordsLibrary.add("cat");
+        wordsLibrary.add("table");
+        wordsLibrary.add("car");
+        wordsLibrary.add("baseball");
+        wordsLibrary.add("toyota");
+        wordsLibrary.add("pen");
+        wordsLibrary.add("window");
+        wordsLibrary.add("lasagne");
+    }
+    public void initUnknownWord(){
+        unknownWord = new char[theWord.length()];
+
+        for(int i = 0; i<theWord.length(); i++){
+            unknownWord[i] = '_';
+        }
     }
 
     public void pickRandomWord(){
         Random ran = new Random();
-        int randomNum = ran.nextInt((words.size()));
-        theWord = words.elementAt(randomNum);
+        int randomNum = ran.nextInt((wordsLibrary.size()));
+        theWord = wordsLibrary.elementAt(randomNum);
         Log.d(TAG, "pickRandomWord: "+theWord);
 
     }
     public void nextRound(){
         failNmr++;
     }
+
+    public boolean checkWin(){
+        String guessWord = new String(unknownWord);
+        Log.d(TAG, "checkWin: guessWord: "+guessWord+ ", theWord: "+theWord);
+        if(guessWord.equals(theWord)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
     public boolean checkLetterContains(String a){
         if(theWord.contains(a.toLowerCase())){
             Log.d(TAG, "checkLetter contains: "+a);
@@ -47,15 +71,35 @@ public class gameHandler {
             return false;
         }
     }
+    public char[] insertCorrectChars(char c){
+
+        char b = Character.toLowerCase(c);
+
+        for (int i = 0; i<theWord.length(); i++){
+            Log.d(TAG, "insertCorrectChars: comparing: "+theWord.charAt(i)+" & "+b);
+            if(theWord.charAt(i) == b){
+                unknownWord[i] = b;
+                Log.d(TAG, "insertCorrectChars: hit on i = "+i);
+            }
+        }
+        Log.d(TAG, "insertCorrectChars: "+ Arrays.toString(unknownWord));
+        return unknownWord;
+    }
+    public char[] getUnknownWord(){
+        return unknownWord;
+    }
     public int getRound(){
         return failNmr;
     }
+
     public int getWordCount(){
         int c = theWord.length();
         Log.d(TAG, "getWordCount: "+c);
         return c;
     }
-    public void resetGame(){
 
+    public void resetGame(){
+        Intent intent = new Intent(this, activity_menu.class);
+        startActivity(intent);
     }
 }
